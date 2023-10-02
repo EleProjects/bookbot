dbfb637367aac955789d707e307afe2d5ea132e9
EleProjects <kyle541994@yahoo.com>
2023-10-01 22:34:06 -0500

    create function to determine number of each character (a-z) within the text

--- main.py
+++ main.py
@@ -4,8 +4,18 @@ with open("books/frankenstein.txt") as f:
 def num_words(book_text):
     words = file_contents.split()
     total_words = 0
+
     for i in range(0, len(words)):
         total_words += 1
-    print(total_words)
+    return(total_words)
 
-num_words(file_contents)
+def num_of_char(book_text):
+    characters = {}
+
+    for char in book_text.lower():
+        if char.isalpha() and char not in characters:
+            characters[char] = 1
+        elif char.isalpha() and char in characters:
+            characters[char] += 1
+    return(characters)
+    
