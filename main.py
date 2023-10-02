d3f7def1b7b3dc79f1f70da404c6a2efa33f6c04
EleProjects <kyle541994@yahoo.com>
2023-10-02 01:24:28 -0500

    converts and sorts dictionary to be printed out as data

--- main.py
+++ main.py
@@ -18,4 +18,17 @@ def num_of_char(book_text):
         elif char.isalpha() and char in characters:
             characters[char] += 1
     return(characters)
-    
+
+def get_value(item):
+    return item[1]
+
+num_words = num_words(file_contents)
+char_report = sorted(num_of_char(file_contents).items(), key = get_value, reverse = True)
+
+
+print(f"--- Begin report of {f.name} ---")
+print(f"{num_words} words found in the document\n")
+for i in range(0, len(char_report)):
+    print(f"The '{char_report[i][0]}' character was found {char_report[i][1]} times")
+
+print("--- End report ---")
